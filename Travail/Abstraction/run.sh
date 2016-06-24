e0ffef79ea32996ff8fef6189f0821f012cdaa0a
Picard Michaël <m.picard287@laposte.net>
2016-06-24 17:22:29 +0200

    MAJ pour rapport d'expé avant rapport de Stage

--- Travail/Abstraction/run.sh
+++ Travail/Abstraction/run.sh
@@ -5,5 +5,5 @@
 mv Resultat.csv Resultat_CPU.csv
 ./test.sh 10 10 1000 32 1 GPU1 -G
 ./test.sh 10 10 1000 32 2 GPU2 -G
-./test.sh 10 10 1000 32 3 GPU2 -G
+./test.sh 10 10 1000 32 3 GPU3 -G
 mv Resultat.csv Resultat_GPU.csv
